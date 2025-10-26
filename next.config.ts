6d1ff33e1ab8345783547740c99a1211567943a7
Syed Hamza Ali Naqvi <syedhamzaalinaqvi4@gmail.com>
2025-10-26 07:07:29 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pps.whatsapp.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
