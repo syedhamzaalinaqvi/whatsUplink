999d1151d50b0ac3dd74f1e1d6799a8c81393017
syedhamzaalinaqvi <107138453+syedhamzaalinaqvi@users.noreply.github.com>
2025-10-27 18:19:20 +0500

    updating secrets secure

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAXA-mRInS2LqWzxU43eKNbVl_l-WQZang",
-  authDomain: "moovie-35706.firebaseapp.com",
-  projectId: "moovie-35706",
-  storageBucket: "moovie-35706.appspot.com",
-  messagingSenderId: "1046379679363",
-  appId: "1:1046379679363:web:f37ebc4a08008f58d695b6",
-  measurementId: "G-DRMJYG3RW5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
