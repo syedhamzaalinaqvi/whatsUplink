a3917659f86cfd0dc9861f036c2b415e464ffbec
Syed Hamza Ali Naqvi <syedhamzaalinaqvi4@gmail.com>
2025-10-27 05:29:09 +0000

    try to fix this error "Error: Invalid src prop (https://static.whatsapp.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'pps.whatsapp.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.whatsapp.net',
         port: '',
         pathname: '/**',
       }
