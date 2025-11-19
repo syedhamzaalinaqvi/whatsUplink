cbb968025137091676163268b94e18e0dc1152e6
Syed Hamza Ali Naqvi <syedhamzaalinaqvi4@gmail.com>
2025-11-19 14:54:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -124,9 +124,9 @@ export function Header({
             <NavigationMenuList>
                 {navLinks.filter(link => link.href === '/').map(link => (
                     <NavigationMenuItem key={link.id}>
-                        <Link href={link.href} legacyBehavior={false} passHref>
-                          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                              {link.label}
+                        <Link href={link.href} legacyBehavior passHref>
+                          <NavigationMenuLink asChild>
+                              <a className={navigationMenuTriggerStyle()}>{link.label}</a>
                           </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
@@ -172,9 +172,9 @@ export function Header({
 
                  {navLinks.filter(link => !['/', '/submit'].includes(link.href)).map(link => (
                     <NavigationMenuItem key={link.id}>
-                         <Link href={link.href} legacyBehavior={false} passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                {link.label}
+                         <Link href={link.href} legacyBehavior passHref>
+                            <NavigationMenuLink asChild>
+                                <a className={navigationMenuTriggerStyle()}>{link.label}</a>
                             </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
